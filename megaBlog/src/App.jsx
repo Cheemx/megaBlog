1742ae24957733554f2b4350a7ca80677d69e78a
Cheemx <chinmaymahajan999@gmail.com>
2024-07-23 18:51:30 +0530

    Authorization for appwrite configured

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Header, Footer} from './components'
 
 function App() {
 
-  return (
-    <>
-    <h1>Blog App with Appwrite </h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-700'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+           {/* We will Handle Outlwt later : <Outlet />  */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
