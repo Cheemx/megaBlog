87e1e2b0d89fa2b2db7fafaeedc6675ff84dbc83
Cheemx <chinmaymahajan999@gmail.com>
2024-07-28 23:26:06 +0530

    .

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 import { useDispatch } from 'react-redux';
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
-import {Header, Footer} from './components'
+// import {Header, Footer} from './components'
+import Header from './components/Header/Header'
+import  Footer  from './components/Footer/footer';
 import { Outlet } from 'react-router-dom';
 
 function App() {
