8cb32a195752db940f94031a573872af4c9eadb7
Cheemx <chinmaymahajan999@gmail.com>
2024-07-29 00:14:41 +0530

    .

--- megaBlog/src/main.jsx
+++ megaBlog/src/main.jsx
@@ -20,7 +20,7 @@ import AllPosts from './pages/AllPosts.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App />,  
     children: [
         {
             path: "/",
